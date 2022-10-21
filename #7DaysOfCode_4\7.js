e67a82760b20baeda20a5aed1a70bd5b97d75c96
SamuelMacF <115838498+SamuelMacF@users.noreply.github.com>
2022-10-21 18:50:49 -0300

    Resolução do desafio do dia 4

---
+++ #7DaysOfCode_4\7.js
@@ -0,0 +1,22 @@
+alert("Olá, seja bem vindo")
+alert("Vamos jogar um jogo?")
+
+const numeroAleatorio = Math.floor(Math.random() * (10 - 0 + 1) + 0)
+let numeroDoUsuario;
+let acertou = false;
+  
+ for (let numeroDeTentativas = 0; numeroDeTentativas < 3 ; numeroDeTentativas ++){
+     numeroDoUsuario = prompt("Você tem 3 tentativas para adivinhar o numero que eu escolhi. Qual número de 0 a 10 você deseja chutar?")
+   
+    if(numeroDoUsuario == numeroAleatorio){
+      alert (`Parabéns você acertou, o número era ${numeroAleatorio}!`)
+      acertou = true;
+      break;
+    }
+   
+   alert("Você errou, tente novamente!");
+}
+
+if (!acertou){
+  alert(`Infelizmente, você não acertou. O número era ${numeroAleatorio}!`);
+}
